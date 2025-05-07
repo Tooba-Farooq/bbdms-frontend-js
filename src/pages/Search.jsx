1ebdd46361baac29ba31a363347e044c6e720a6d
Tooba-Farooq <toobafar004@gmail.com>
2025-05-07 06:10:10 +0500

    Removed checks from Search bar in search.jsx

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/*import React, { useState } from "react";
 import "../styles/Search.css";
 
 import aghaKhanImage from "../assets/images/agha khan.png";
@@ -50,4 +50,55 @@ const Search = () => {
   );
 };
 
+export default Search;*/
+
+import React, { useState } from "react";
+import "../styles/Search.css";
+
+import aghaKhanImage from "../assets/images/agha khan.png";
+import liaquatHospitalImage from "../assets/images/liaquat hospital.png";
+import JPMCImage from "../assets/images/jpmc.png";
+import FatimidImage from "../assets/images/fatimid.png";
+import NICVDImage from "../assets/images/nicvd.png";
+import HusainiImage from "../assets/images/husaini.png";
+import chughtaiImage from "../assets/images/chughtai.png";
+import PWAImage from "../assets/images/pwa.png";
+
+const hospitals = [
+  { name: "Agha Khan Hospital", image: aghaKhanImage },
+  { name: "Liaquat National Hospital", image: liaquatHospitalImage },
+  { name: "Jinnah Medical Center", image: JPMCImage },
+  { name: "Fatimid Foundation Blood Bank", image: FatimidImage },
+  { name: "National Institute of Cardivascular Disease (NICVD)", image: NICVDImage },
+  { name: "Husaini Blood Bank", image: HusainiImage },
+  { name: "Chughtai Blood Center", image: chughtaiImage },
+  { name: "PWA Blood Bank", image: PWAImage },
+];
+
+const Search = () => {
+  const [search, setSearch] = useState("");
+
+  return (
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder="Search hospital/ blood"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className="search-bar"
+      />
+
+      <div className="hospital-grid">
+        {hospitals.map((h, i) => (
+          <div className="hospital-card" key={i}>
+            <img src={h.image} alt={h.name} />
+            <h4>{h.name}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
 export default Search;
+
